2ed0caa55facfe8a287b1de62ee0988cdf4ab280
temtuba8-jpg <temtuba8@gmail.com>
2026-09-20 20:26:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config["SECRET_KEY"] = "kawahla_secret_key_2026_super_secure"
 
 # إعداد اتصال MongoDB السحابي مع معايير الأمان والتوافق مع Render
 app.config["MONGO_URI"] = (
-    "mongodb+srv://hzoooof11_db_user:Fad%400911923356@cluster0.outspyb.mongodb.net/kawahla_db?retryWrites=true&w=majority&appName=Cluster0&tls=true&tlsAllowInvalidCertificates=true"
+    "mongodb+srv://kawahla:Fad%400911923356@cluster0.outspyb.mongodb.net/kawahla_db?retryWrites=true&w=majority&appName=Cluster0&tls=true&tlsAllowInvalidCertificates=true"
 )
 
 # مفتاح ImgBB المباشر لرفع الصور والملفات (يفصل الصور عن السيرفر لتوافق Render)
