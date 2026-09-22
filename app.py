0a82fd867d5fae77673a747cb1cb0cb0d382d7e4
temtuba8-jpg <temtuba8@gmail.com>
2026-09-22 17:42:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 import os
-from urllib.parse import quote_plus
 
 from bson.objectid import ObjectId
 from flask import (
@@ -35,28 +34,22 @@ app.config["SECRET_KEY"] = "kawahla_secret_key_2026_super_secure"
 # إعداد اتصال MongoDB السحابي
 # =========================================================
 
-MONGO_USERNAME = "kawahla"
-MONGO_PASSWORD = quote_plus("Fad%[400911923356")
-
-# إعداد اتصال MongoDB السحابي
-# =========================================================
-# إعداد اتصال MongoDB السحابي
-# =========================================================
-
 app.config["MONGO_URI"] = (
     "mongodb+srv://kawahla:Fad%400911923356@cluster0.outspyb.mongodb.net/"
     "kawahla_db?retryWrites=true&w=majority&appName=Cluster0"
     "&tls=true&tlsAllowInvalidCertificates=true"
 )
 
+
 IMGBB_API_KEY = "cc5590cadcc0d03fc63bbb5317442839"
 
+
 mongo = PyMongo(app)
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
-
 # =========================================================
 # رفع الصور إلى ImgBB
 # =========================================================
